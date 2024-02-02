305439f27527ea77f08cd50667d668c93ecd2359
tasyasoni <tasyasoni1@gmail.com>
2024-02-02 22:54:31 +0300

    'correct_final_26.2'

--- lessonsapp/services.py
+++ lessonsapp/services.py
@@ -2,7 +2,7 @@ from celery import shared_task
 from django.core.mail import send_mail
 from config import settings
 
-
+@shared_task
 def sendmail():
     send_mail(
         'Подписка на обновления курса',
